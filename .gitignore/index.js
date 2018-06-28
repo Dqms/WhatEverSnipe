51a88476fb88e26a64447b10b1cc335f294b72e7
Dqms <40544467+Dqms@users.noreply.github.com>
2018-06-28 16:42:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,4 +8,12 @@ bot.login(process.env.TOKEN);
 bot.on('ready', function() {
     bot.user.setGame("Ready :)")
     console.log("Connected");
+    
+module.exports.run = async (bot, message, args) => {
+
+    Let msg = ready message.channel.send("Ready");
+    ready msg.react("✅");
+    ready msg.react("❎");
+    
+}
 });
